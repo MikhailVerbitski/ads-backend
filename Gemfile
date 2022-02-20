10a20692d95b59ac75aeaeee91aebebdfab17311
MikhailVerbitski <misha.verbi@gmail.com>
2022-02-20 22:51:49 +0100

    configured user model

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'pry-byebug'
 
 gem "devise", "~> 4.8"
 gem 'rack-cors'
+gem 'enumerize', '~> 2.5.0'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,21 @@
 class User < ApplicationRecord
+  extend Enumerize
+  enumerize :role, in: %i[guest user admin], default: :guest
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :trackable,
+         :timeoutable,
+         :validatable,
+         :confirmable
+
+  before_save :change_role
+
+  def change_role
+    self.role = :user
+  end
 end
