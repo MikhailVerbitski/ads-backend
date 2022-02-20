fb104c8987a15f3a998b60ba0a07854a44b9f82f
MikhailVerbitski <misha.verbi@gmail.com>
2022-02-20 23:19:11 +0100

    init user and post controllers

---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,31 @@
+class Api::V1::PostsController < ApplicationController
+  # def new; end
+  # def edit; end
+  def show
+  end
+
+  def create
+    # if @post.save
+    #   redirect_to @post
+    # else
+    #   flash.alert = "Can't save!"
+    # end
+  end
+
+  def update
+    # if @post.update(create_params)
+    #   redirect_to @post
+    # else
+    #   flash.alert = "Can't Update!"
+    # end
+  end
+
+  def destroy
+    # @post.delete
+    # redirect_to root_url, notice: 'Post deleted'
+  end
+
+  def index
+    render json: Post.all
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :posts
+  namespace :api do
+    namespace :v1 do
+      resources :posts
+    end
+  end
 end
