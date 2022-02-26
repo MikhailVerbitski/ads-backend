a85ce6c317c914fe056bc98c3db3349320ddea64
MikhailVerbitski <misha.verbi@gmail.com>
2022-02-27 00:17:54 +0100

    bf

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,8 +3,11 @@ class Users::SessionsController < Devise::SessionsController
 
   private
 
-  def respond_with(resource, _opts = {})
-    render json: { message: 'Logged.' }, status: :ok
+  def respond_with(_resource, _opts = {})
+    render json: {
+      message: 'You are logged in.',
+      user: current_user
+    }, status: :ok
   end
 
   def respond_to_on_destroy
@@ -12,10 +15,10 @@ class Users::SessionsController < Devise::SessionsController
   end
 
   def log_out_success
-    render json: { message: "Logged out." }, status: :ok
+    render json: { message: 'You are logged out.' }, status: :ok
   end
 
   def log_out_failure
-    render json: { message: "Logged out failure."}, status: :unauthorized
+    render json: { message: 'Hmm nothing happened.' }, status: :unauthorized
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,7 @@
 class User < ApplicationRecord
-  extend Enumerize
-  enumerize :role, in: %i[guest user admin], default: :guest
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise  :database_authenticatable, :registerable,
-          :recoverable,
-          :rememberable,
-          :trackable,
-          :timeoutable,
-          :validatable,
-          :confirmable,
-          :jwt_authenticatable,
-          jwt_revocation_strategy: JwtDenylist
-
-  # before_save :change_role
-
-  # def change_role
-  #   self.role = :user
-  # end
+  devise :database_authenticatable, :registerable,
+         :jwt_authenticatable,
+         jwt_revocation_strategy: JwtDenylist
 end
